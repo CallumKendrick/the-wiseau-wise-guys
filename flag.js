45876f55b8015094cc786a78274f876b02efc35b
KirkimusII <kirk2242@gmail.com>
2017-03-26 10:55:48 +0100

    before going to sleep

--- flag.js
+++ flag.js
@@ -1,20 +1,37 @@
 var flag = function(data)
 {
+   console.log("changed")
     var blockedWords = data.blockedWords;
-    var text = blockedWords.join(" ");
-    $("body").text(text)
-    
-}
-
-var change = function()
-{
-    $("message_body").each(flag)
-    $(this)
-    
+    $(".message_body").each(function()
+    {
+        for(var i = 0; i < blockedWords.length; i++)
+        {
+            console.log($(this));
+            var wordToCheckFor = blockedWords[i];
+            var wordIsInMessage = $(this).text().toLowerCase().indexOf(wordToCheckFor) != 0
+            console.log($(this).text().toLowerCase());
+            
+            console.log(wordIsInMessage);
+            
+            if(wordIsInMessage)
+            {
+                console.log("hai")
+                //$(this).text("I am sick of this world");
+            }
+        }
+    })
 }
 
 var getWords = function()
 {
     chrome.storage.sync.get("blockedWords", flag);
 }
-$(getWords)
+
+
+//$(getWords)
+$(function()
+{
+  console.log("oh hi mark")
+    //$("#msgs_div").bind("DOMSubtreeModified", getWords);  
+    $("#messages_container").
+})
